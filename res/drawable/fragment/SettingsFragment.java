51940ceaa49be8486a45f3ba37ff66bc2502aade
BCP <75612924+GianlucaPanzani@users.noreply.github.com>
2022-10-14 17:19:08 +0200

    resource files

---
+++ res/drawable/fragment/SettingsFragment.java
@@ -0,0 +1,20 @@
+package it.unipi.di.sam.m550358.trekkfit.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import it.unipi.di.sam.m550358.trekkfit.R;
+
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup vg, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, vg, false);
+    }
+    
+}
